363c5b61f55359aba72471f67bb2d93145af7eda
arpitgupta1906 <gupta.25.iitj.ac.in>
2020-03-25 00:39:32 +0530

    added post and groups app

---
+++ socialclone/groups/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
+# Create your views here.
+from django.urls import reverse
+from django.views import generic
+from .models import Group,GroupMember
+
+class CreateGroup(LoginRequiredMixin,generic.CreateView):
+    fields=('name','description')
+    model=Group
+
+
+
+class SingleGroup(generic.DetailView):
+    model=Group
+
+
+class ListGroups(generic.ListView):
+    model=Group
---
+++ socialclone/posts/urls.py
@@ -0,0 +1,16 @@
+from . import views 
+from django.urls import path,include
+from django.conf.urls import url
+
+app_name='posts'
+
+
+urlpatterns = [
+    path('',views.PostList.as_view(),name='all'),
+    path('new/',views.CreatePost.as_view(),name='create'),
+    url(r'by/(?P<username>[-\w]+)/$',views.UserPosts.as_view(),name='for_user'),
+    url(r'by/(?P<username>[-\w]+)/(?P<pk>\d+)/$',views.PostDetail.as_view(),name='single'),
+    url(r"delete/(?P<pk>\d+)/$",views.DeletePost.as_view(),name="delete"),
+
+
+]
