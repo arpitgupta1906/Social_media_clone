6e5775811a64444fa281322200735f080fcc230e
arpitgupta1906 <gupta.25.iitj.ac.in>
2020-03-25 14:13:48 +0530

    finished the basic functionality

--- socialclone/socialclone/settings.py
+++ socialclone/socialclone/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'accounts',
+    'groups',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -121,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR=[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
 
 LOGIN_REDIRECT_URL='test'
--- socialclone/socialclone/urls.py
+++ socialclone/socialclone/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path('accounts/',include('accounts.urls',namespace='accounts')),
     path('accounts/',include('django.contrib.auth.urls')),
     path('test/',views.TestPage.as_view(),name='test'),
-    path('thanks/',views.ThanksPage.as_view(),name='thanks')
+    path('thanks/',views.ThanksPage.as_view(),name='thanks'),
+    path('posts/',include('posts.urls',namespace='posts')),
+    path('groups/',include('groups.urls',namespace='groups'))
 ]
